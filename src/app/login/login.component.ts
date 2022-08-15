8e14c1daa6f7104a2012fab1fa2c4aeecb3dd53e
amalnathvp <amalnathvp31@gmail.com>
2022-08-15 16:11:21 +0530

    first commit

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Route, Router } from '@angular/router';
+import { AuthService } from '../service/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -9,9 +11,11 @@ export class LoginComponent implements OnInit {
 
   username = "";
   password = "";
-  errorMsg = ""
+  errorMsg = "";
 
-  constructor() { }
+  constructor(private auth: AuthService, private router: Router) {
+
+   }
 
   ngOnInit(): void {
   }
@@ -21,6 +25,15 @@ export class LoginComponent implements OnInit {
       this.errorMsg = "Username is required";
     } else if (this.password.trim().length === 0) {
       this.errorMsg = "Password is required";
+    } else {
+      this.errorMsg = "";
+      let res = this.auth.login(this.username, this.password);
+      if (res === 200) {
+        this.router.navigate(['home']);
+      }
+      if (res === 403) {
+        this.errorMsg = "Invalid Credential"
+      }
     }
   }
 
